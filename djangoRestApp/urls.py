f513c31792de816f260a825afb81e75c42cef0b4
BalayetHosain <balayet.kafo@gmail.com>
2021-06-20 22:48:22 +0600

    class base view

--- djangoRestApp/urls.py
+++ djangoRestApp/urls.py
@@ -1,8 +1,11 @@
 
 from django.urls import path
-from djangoRestApp.views import article_list, article_detail
+# from djangoRestApp.views import article_list, article_detail
+from djangoRestApp.views import Articla_list, ArticleDetails
 
 urlpatterns = [
-    path('articleListAPI/', article_list, name='articleListAPI'),
-    path('article_detailAPI/<int:pk>/', article_detail, name='article_detailAPI'),
+    # path('articleListAPI/', article_list, name='articleListAPI'),
+    # path('article_detailAPI/<int:pk>/', article_detail, name='article_detailAPI'),
+    path('Articla_listAPI/', Articla_list.as_view(), name='Articla_listAPI'),
+    path('ArticleDetailsAPI/<int:pk>/', ArticleDetails.as_view(), name='ArticleDetailsAPI'),
 ]
--- djangoRestApp/views.py
+++ djangoRestApp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework import serializers
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
@@ -9,6 +9,8 @@ from djangoRestApp.serializers import ArticleSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.renderers import TemplateHTMLRenderer
 #Function Based API Views
 # @csrf_exempt
 # def article_list(request):
@@ -49,6 +51,51 @@ from rest_framework.response import Response
 #         artcle.delete()
 #         return HttpResponse(status=201)
 
+#Class Based Views
+class Articla_list(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'article.html'
+
+    def get(self, request):
+        artle = Article.objects.all()
+        serializer = ArticleSerializer(artle, many=True)
+        return Response({'artle':artle,'serializer':serializer})
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ArticleDetails(APIView):
+    def get_object(self, pk):
+        try:
+            return get_object_or_404(Article, pk=pk)
+        except Article.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        artle = self.get_object(pk)
+        serializer = ArticleSerializer(artle)
+        return Response(serializer.data)
+
+
+    def put(self, request, pk):
+        artle = self.get_object(pk)
+        serializer = ArticleSerializer(artle, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        artle = self.get_object(pk)
+        artle.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+"""
 #api view Decorator
 @api_view(['GET', 'POST'])
 def article_list(request):
@@ -88,4 +135,4 @@ def article_detail(request, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-
+"""
